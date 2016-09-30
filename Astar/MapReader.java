4468f037c9ec2dbbf320a538032bfcfd99c105a3
ulvestad <simenul@stud.ntnu.no>
2016-09-30 20:53:12 +0200

    added board height and width

--- Astar/MapReader.java
+++ Astar/MapReader.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 public class MapReader {
 
 	private String map = "";
+	private int boardHeight = 0;
+	private int boardWidht = 0;
 
 	public String getMap(){
 		return map;
@@ -33,11 +35,23 @@ public class MapReader {
 		int xpos = (int) getIndex('B')%21;
 		return xpos;
 	}
+	public int getBoardHeight(){
+		return boardHeight;
+	}
+	public int getBoardWidth(){
+		return boardWidht;
+	}
 
 	public void readFromFile(String path) throws IOException{
 		BufferedReader reader = new BufferedReader(new FileReader(path));
 		int line;
+		int counter = 0;
 		while ((line = reader.read()) != -1) {
+			counter +=1;
+			if ((char)line == '\n'){
+				boardHeight +=1;
+				boardWidht = counter;
+			}
 			map += (char)line;
 		}
 		reader.close();
--- Astar/README.md
+++
@@ -1,6 +0,0 @@
-# A* algorithm
-A* algorithm created in java for NTNU course TDT4136 (Introduction to AI)
-
-MapReader.java reads and translates the map from .txt files <br />
-AStar.java uses the A* algorithm with Manhatten heuristics to find the shortest path from A to B
-
